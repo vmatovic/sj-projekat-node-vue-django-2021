bb07b2b5ec4ead9c73e4c740c2fefe3def050e2a
vmatovic <vmatovic3818rn@raf.rs>
2021-02-12 14:33:47 +0100

    Hello world verzija servera, napravljen vue klijent

---
+++ server_node/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(morgan('tiny'));
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Radi nesto'
+    });
+});
+
+app.listen(2999, () => {
+    console.log('Port 2999 aktivan');
+});
