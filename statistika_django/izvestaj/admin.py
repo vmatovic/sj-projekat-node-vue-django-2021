1475f5be7bdeb29876e36e630a93785de429c53a
vmatovic <vmatovic3818rn@raf.rs>
2021-02-16 12:58:56 +0100

    Dodatak

--- statistika_django/izvestaj/admin.py
+++ statistika_django/izvestaj/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Dugmici, Korisnici, Materijali, MaterijaliNamestaj, Narudzbina, Komentari
 
 # Register your models here.
+admin.site.register(Dugmici)
+admin.site.register(Korisnici)
+admin.site.register(Materijali)
+admin.site.register(MaterijaliNamestaj)
+admin.site.register(Narudzbina)
+admin.site.register(Komentari)
